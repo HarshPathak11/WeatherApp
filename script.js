823f2b22588c5e11b8d1f5ea69e97b1f24111c32
Harsh Pathak <jkpathak83195@gmail.com>
2023-10-08 15:08:25 +0530

    improved design and functionality

--- script.js
+++ script.js
@@ -5,18 +5,27 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 const weather = document.querySelector(".weather");
 const dets = document.querySelector(".details");
-
+let flag=0;
 async function check(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`); // Added "&" before "appid"
     const data = await response.json();
-
+    if(data.name==undefined)
+    {    
+    document.querySelector(".city").innerHTML = "Please enter a valid city!";
+    document.querySelector(".temp").innerHTML = "-" + "°C";
+    document.querySelector(".humidity").innerHTML = "-" + "%";
+    document.querySelector(".wind").innerHTML = "-"+ "Km/h";
+    flag=1;
+    
+    }
+    else{
     console.log(data);
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
-
-
+    }
+    if(flag==0){
     if (data.weather[0].main == "Clouds") {
         weatherIcon.src = "./Images/clouds.png";
     } else if (data.weather[0].main == "Clear") {
@@ -29,12 +38,17 @@ async function check(city) {
         weatherIcon.src = "./Images/drizzle.png";
     } else if (data.weather[0].main == "Snow") {
         weatherIcon.src = "./Images/snow.png";
-    }
+    }}
     weather.style.display = "block";
     dets.style.display = "flex";
-
-
+    
     search.value = data.name;
+    if(flag==1){
+        weatherIcon.src = "";
+        search.value = "";
+    }
+    flag=0;
+
 }
 
 searchBtn.addEventListener("click", () => {
